a95109779f5dbfa34d3276ee6fe9c7ac2654879d
Heisenberg <Persey_98@list.ru>
2020-04-10 21:12:27 +0500

    beginning of project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework9._1_IPlayable_IRecordable
+{
+
+    interface IPlayable
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
