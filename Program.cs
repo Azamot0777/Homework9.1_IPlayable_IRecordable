0da38d3c1ba98495d80a4cf25947b613a076f1c8
Heisenberg <Persey_98@list.ru>
2020-04-10 21:28:05 +0500

    I've created 2 interface (IPlayable , IRecordable)

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 
-namespace Homework9._1_IPlayable_IRecordable
+namespace Homework9_1_IPlayable_IRecordable
 {
 
     interface IPlayable
     {
+        public void Play();
+        public void Pause();
+        public void Stop();
+
+    }
+    interface IRecordable
+    {
+        public void Record();
+        public void Pause();
+        public void Stop();
+    }
+
+    class Player : IPlayable, IRecordable
+    {
+        public void Play()
+        {
+            System.Console.WriteLine("Play");
+        }
+        public void Record()
+        {
+            System.Console.WriteLine("Record");
+        }
+        public void Pause()
+        {
+            System.Console.WriteLine("Pause");
+        }
+        public void Stop()
+        {
+            System.Console.WriteLine("Stop");
+        }
 
     }
     class Program
